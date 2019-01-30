b47028bd80efd651b48610979b9d68d23fc2efb7
Ayla Kovner <a.kovner.1746@gmail.com>
2019-01-30 13:20:50 -0500

    lab 2

---
+++ lab2/App.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, TextInput, Button } from 'react-native';
+
+export default class App extends React.Component {
+	constructor(props) {
+    	super(props);
+		this.onPress = this.onPress.bind(this);
+		this.state = {value: ''};
+		this.name = {value: ''};
+    	this.submitted = false;
+		this.viable = false;
+
+		this.onChange = this.onChange.bind(this);
+  	}
+  	onChange(event) {
+		console.log(event);
+		this.state = {value: event};
+  	}
+  
+  	onPress(event) {
+		console.log("Pressed");
+		this.submitted = true;
+		console.log(this.state);
+		console.log(this.submitted);
+		const tester = /^[a-zA-Z\s]*$/;
+
+		if((tester.test(this.state.value))==false){
+			this.viable = false;
+		}
+		else{
+			this.viable = true;
+		}
+		console.log(this.viable);
+		console.log(this.state);
+		event.preventDefault;
+		console.log(this.state);
+		this.name = this.state.value;
+		this.setState({state: this.state});
+		console.log(this.state);
+	}
+	
+  	render() {		
+		const displayQ = this.submitted;
+		const displayName = this.viable;
+		console.log(this.name);
+
+		if(displayQ == true){
+			if(displayName == false){
+				console.log("Display Wrong");
+				return (
+					<Text style={styles.youreWrong}>Please use only letters and spaces</Text>
+				);
+			}
+			else if(displayName == true){
+				console.log("Display Name");
+				console.log(this.name.value);
+				return (
+					<Text style={styles.worksFine}>{'Hi '}{this.name}</Text>
+				);
+			}
+		}
+		else if(displayQ == false){
+			console.log("Display Question")
+			return (
+				<View style={styles.container} flexDirection="column" alignItems='stretch'>
+				<View><TextInput style={styles.textInput} onChangeText={this.onChange} placeholder="Enter your name"></TextInput></View>
+				<TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}><Text style={styles.buttonText}>Submit</Text></TouchableOpacity>
+				</View>
+			);
+		}
+  	}
+}
+
+const styles = StyleSheet.create({
+	buttonText:
+	{
+    	color:"white",
+    	fontSize:40
+  	},
+  	buttonStyle:
+  	{
+    	justifyContent: 'center',
+    	alignItems: 'center',
+    	backgroundColor: '#EC4DCD',
+	    height:75,
+		margin:30,
+		borderRadius:15,
+  	},
+  	textInput:
+  	{
+    	margin:30,
+    	height:75,
+    	fontSize:20
+  	},
+  	defaultText:
+  	{
+    	fontSize:20
+  	},
+  	container: {
+ 		flex: 1,
+    	backgroundColor: '#fff',
+    	alignItems: 'center',
+    	justifyContent: 'center',
+  	},
+  	youreWrong: {
+		margin: 100,
+		color: 'red',
+		fontSize: 20,
+	},
+	worksFine: {
+		margin: 100,
+		color: 'black',
+		fontSize: 20,
+	}  
+});
